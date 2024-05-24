dcb9794710e289dbd22fb06dbdfe35557c10318b
Анастасия Фергис <f.nastya_102i@mail.ru>
2024-05-24 14:30:33 +1000

    Добавьте файлы проекта.

---
+++ ModelAdd/AppConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_09;
+
+namespace Task_09.ModelAdd
+{
+    internal class AppConnect
+    {
+        public static Task10Entities modelOdb;
+    }
+}
---
+++ ModelAdd/AppFrame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Task_09;
+
+namespace Task_09.ModelAdd
+{
+    internal class AppFrame
+    {
+        public static Frame MainFrame;
+    }
+}
